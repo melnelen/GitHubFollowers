83d0d265394971f2adb77ec7ad7b2eecd0f2f743
Alex Ivanova <melnelen@gmail.com>
2024-03-25 17:26:01 +0100

    add model + bug fix

---
+++ GitHubFollowers/Model/Follower.swift
@@ -0,0 +1,8 @@
+//
+//  Follower.swift
+//  GitHubFollowers
+//
+//  Created by Alexandra Ivanova on 25/03/2024.
+//
+
+import Foundation
---
+++ GitHubFollowers/Model/User.swift
@@ -0,0 +1,8 @@
+//
+//  User.swift
+//  GitHubFollowers
+//
+//  Created by Alexandra Ivanova on 25/03/2024.
+//
+
+import Foundation
