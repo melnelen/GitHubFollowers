1b0e3693eb65b1e9145ba77f0323ba4520593827
Alex Ivanova <melnelen@gmail.com>
2024-03-25 17:26:40 +0100

    add model + bug fix

--- GitHubFollowers/Model/User.swift
+++ GitHubFollowers/Model/User.swift
@@ -6,3 +6,17 @@
 //
 
 import Foundation
+
+struct User: Codable {
+    var login: String
+    var avatarUrl: String
+    var htmlUrl: String
+    var name: String?
+    var location: String?
+    var bio: String?
+    var publicRepos: Int
+    var publicGists: Int
+    var followers: Int
+    var following: Int
+    var created_at: String 
+}
